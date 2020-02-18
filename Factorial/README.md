c10a223c8fb8fd2b0dac112bfc63a4b8c1bee68a
nathan-rees <poohplop1@gmail.com>
2020-02-18 09:35:20 +0000

    factorial

---
+++ Factorial/README.md
@@ -0,0 +1,4 @@
+int factorial(int n)
+
+factorial(5)-->120
+factorial(3)-->6
---
+++ Factorial/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args)
+	{
+		
+	}
+	public static int factorial(int n)
+	{
+		int count=1;
+		for(int i=n;i>0;i--) {
+		count=count*i;
+		}
+		return count;
+	}
+	
+}
